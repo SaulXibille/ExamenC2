5b6142803580568c66157414d0e1557caab29d29
SaulXibille <49504706+SaulXibille@users.noreply.github.com>
2020-03-04 18:41:41 -0700

    Ventana principal

--- app/src/main/java/com/example/examenc2/Encuesta1.java
+++ app/src/main/java/com/example/examenc2/Encuesta1.java
@@ -1,15 +1,14 @@
 package com.example.examenc2;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Encuesta1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_encuesta1);
     }
 }
