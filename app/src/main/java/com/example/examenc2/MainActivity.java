f5b8e4140c79f8aa0c725979e305e7952b9c388c
SaulXibille <49504706+SaulXibille@users.noreply.github.com>
2020-03-04 19:30:22 -0700

    Revert "dfgdfh"
    This reverts commit 199a9e65c8a28dbb23d3c9df9ba880e83e91ba25.

--- app/src/main/java/com/example/examenc2/MainActivity.java
+++ app/src/main/java/com/example/examenc2/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.examenc2;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,12 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        try {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-        }catch(Exception e) {
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
