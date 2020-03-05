199a9e65c8a28dbb23d3c9df9ba880e83e91ba25
SaulXibille <49504706+SaulXibille@users.noreply.github.com>
2020-03-04 19:00:02 -0700

    dfgdfh

--- app/src/main/java/com/example/examenc2/MainActivity.java
+++ app/src/main/java/com/example/examenc2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.examenc2;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,7 +10,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }catch(Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
